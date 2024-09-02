aa6a2fc408ae023a9283f07fda16e1d272520a9c
Tanupal05 <tannupundir0@gmail.com>
2024-09-03 00:07:08 +0530

    Initial commit

---
+++ Client/src/main.jsx
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { Toaster } from './components/ui/sonner.jsx';
+import { SocketProvider } from './context/socketContext.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <SocketProvider>
+    <App />
+    <Toaster closeButton/>
+    </SocketProvider>
+)
